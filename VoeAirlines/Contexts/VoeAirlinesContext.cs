40bf530f60bc75c0c2284eabb1ef4b2134395378
cleverlmf <clf.nameless@gmail.com>
2022-08-17 12:00:32 -0300

    first commit

---
+++ VoeAirlines/Contexts/VoeAirlinesContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VoeAirlines.Entities;
+
+namespace VoeAirlines.Contexts
+{
+    
+    public class VoeAirlinesContext : DbContext
+    {
+        
+        public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
+        public DbSet<Cancelamento> Cancelamentos => Set<Cancelamento>();
+        public DbSet<Manutencao> Manutencoes => Set<Manutencao>();
+        public DbSet<Piloto> Pilotos => Set<Piloto>();
+        public DbSet<Voo> Voos => Set<Voo>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {            
+            optionsBuilder.UseSqlServer("Password=teste*123;Persist Security Info=True;User ID=sa;Initial Catalog=master;Data Source=Lab206_17");
+        }
+    }
+}
---
+++ VoeAirlines/Entities/Manutencao.cs
@@ -0,0 +1,24 @@
+﻿using VoeAirlines.Entities.Enums;
+
+namespace VoeAirlines.Entities
+{
+    public class Manutencao
+    {
+        public Manutencao(DateTime dataHora,TipoManutencao tipoManutencao , int aeronaveId, string? observacao = null)
+        {
+
+            DataHora = dataHora;            
+            AeronaveId = aeronaveId;
+            this.tipoManutencao = tipoManutencao;
+            Observacao = observacao;
+        }
+        
+        public int Id { get; set; }
+        public DateTime DataHora { get; set; }
+        public TipoManutencao tipoManutencao { get; set; }
+        public string? Observacao { get; set; }
+        public int AeronaveId { get; set; }
+       
+        public Aeronave Aeronave { get; set; } = null!;
+    }
+}
