3b2248d8efcad8320c7068aa48f69044796dea29
cleverlmf <clf.nameless@gmail.com>
2022-08-24 11:59:42 -0300

    api/aeronaves

---
+++ VoeAirlines/Services/AeronaveService.cs
@@ -0,0 +1,38 @@
+﻿using VoeAirlines.Contexts;
+using VoeAirlines.Entities;
+using VoeAirlines.ViewModels.Aeronave;
+
+namespace VoeAirlines.Services
+{
+    public class AeronaveService
+    {
+        private readonly VoeAirlinesContext _context;
+
+        public AeronaveService(VoeAirlinesContext context)
+        {
+            _context = context;
+        }
+
+        public DetalhesAeronaveViewModel AdicionarAeronave(AdicionarAeronaveViewModel dados)
+        {
+            var aeronave = new Aeronave(dados.Fabricante, dados.Modelo, dados.Codigo);
+
+            _context.Add(aeronave);
+            _context.SaveChanges();
+
+            return new DetalhesAeronaveViewModel(aeronave.Id, aeronave.Fabricante, aeronave.Modelo, aeronave.Codigo);
+        }
+
+        public DetalhesAeronaveViewModel AtualizarAeronave(AtualizarAeronaveViewModel dados)
+        {
+            var aeronave = new Aeronave(dados.Fabricante, dados.Modelo, dados.Codigo);
+            aeronave.Id = dados.Id;
+
+            _context.Update(aeronave);
+            _context.SaveChanges();            
+
+            return new DetalhesAeronaveViewModel(aeronave.Id, aeronave.Fabricante, aeronave.Modelo, aeronave.Codigo);
+
+        }
+    }
+}
